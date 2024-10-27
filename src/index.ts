b7f489bf768abfdd1b147514e778eccbedef0390
Alberto Benatti <albi.benni8@gmail.com>
2024-10-27 18:46:14 +0100

    content

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+#!/usr/bin/env node
+
+import type { Note } from "./types/type.js";
+
 console.log("hello note");
+
+const note = process.argv[2];
+const newNote: Note = {
+  content: note,
+  id: Date.now(),
+};
+
+console.log(newNote);
