31831b5b467a19ab812992214692608dc5aeed63
Alberto Benatti <albi.benni8@gmail.com>
2024-10-30 11:19:00 +0100

    db

---
+++ src/database/db.ts
@@ -0,0 +1,25 @@
+import fs from "node:fs/promises";
+import type { DB, Note } from "../types/type.js";
+
+const DB_PATH = "/Users/benni/benni-projects/ObsidianVault/QuickNotes/db.json";
+
+export const getDB = async () => {
+  const db = await fs.readFile(DB_PATH, "utf8");
+  return JSON.parse(db);
+};
+
+export const saveDB = async (db: DB) => {
+  await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
+  return db;
+};
+
+export const insertDB = async (note: Note) => {
+  try {
+    const db = await getDB();
+    db.notes.push(note);
+    await saveDB(db);
+    return note;
+  } catch (e: any) {
+    console.error(e.message);
+  }
+};
---
+++ src/database/note.ts
@@ -0,0 +1,39 @@
+import { insertDB, getDB, saveDB } from "./db.js";
+import type { Note } from "../types/type.js";
+
+export const newNote = async (note: string, tags: string[]) => {
+  const newNote: Note = {
+    tags,
+    content: note,
+    id: Date.now(),
+  };
+  await insertDB(newNote);
+  return newNote;
+};
+
+export const getAllNotes = async () => {
+  const { notes } = await getDB();
+  return notes;
+};
+
+export const findNotes = async (filter: string) => {
+  const notes = await getAllNotes();
+  return notes.filter((note: Note) =>
+    note.content.toLowerCase().includes(filter.toLowerCase()),
+  );
+};
+
+export const removeNote = async (id: number) => {
+  const notes = await getAllNotes();
+  const match = notes.find((note: Note) => note.id === id);
+
+  if (match) {
+    const newNotes = notes.filter((note: Note) => note.id !== id);
+    await saveDB({ notes: newNotes });
+    return id;
+  }
+};
+
+export const removeAllNotes = async () => {
+  await saveDB({ notes: [] });
+};
